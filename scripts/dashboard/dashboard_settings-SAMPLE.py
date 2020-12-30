6da1c4de0dd546cb7c5def1140a50d906913ba53
KF7EEL <kf7eel@qsl.net>
2020-12-30 09:10:23 -0800

    update web dashboard, add template

---
+++ scripts/dashboard/dashboard_settings-SAMPLE.py
@@ -0,0 +1,27 @@
+###############################################################################
+#   HBLink - Copyright (C) 2020 Cortney T. Buffington, N0MJS <n0mjs@me.com>
+#   GPS/Data - Copyright (C) 2020 Eric Craw, KF7EEL <kf7eel@qsl.net>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software Foundation,
+#   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+###############################################################################
+
+'''
+Settings for web dashboard.
+'''
+
+# Title of the Dashboard
+dashboard_title = 'HBLink3 D-APRS Dashboard'
+# Logo used on dashboard page
+logo = 'https://raw.githubusercontent.com/kf7eel/hblink3/gps/HBlink.png'
