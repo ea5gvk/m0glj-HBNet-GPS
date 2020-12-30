e362fa73205ca717f59e870af8e6112c9a852b41
KF7EEL <kf7eel@qsl.net>
2020-12-30 09:26:41 -0800

    update web dashboard, add template

--- scripts/dashboard/dashboard.py
+++ scripts/dashboard/dashboard.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-#
 ###############################################################################
 #   HBLink - Copyright (C) 2020 Cortney T. Buffington, N0MJS <n0mjs@me.com>
 #   GPS/Data - Copyright (C) 2020 Eric Craw, KF7EEL <kf7eel@qsl.net>
@@ -23,75 +21,96 @@
 This is a web dashboard for the GPS/Data application.
 '''
 
-from flask import Flask
+from flask import Flask, render_template
 import ast, os
+from dashboard_settings import *
 
 app = Flask(__name__)
-dash_entries = ast.literal_eval(os.popen('cat /tmp/gps_data_user_loc.txt').read())
-#dash_bb = ast.literal_eval(os.popen('cat /tmp/gps_data_user_bb.txt').read())
 
-def get_data():
-    dash_loc = ast.literal_eval(os.popen('cat /tmp/gps_data_user_loc.txt').read())
-    dash_bb = ast.literal_eval(os.popen('cat /tmp/gps_data_user_bb.txt').read())
-    tmp_bb = ''
-    tmp_loc = ''
-    tbl_hdr = '''
+
+tbl_hdr = '''
 <table style="border-color: black; margin-left: auto; margin-right: auto;" border="2" cellspacing="6" cellpadding="2"><tbody>
 '''
-    tbl_ftr = '''
+tbl_ftr = '''
 </tbody>
 </table>
 '''
-    bb_hdr = '''
-<tr>
-<td style="text-align: center;">
-<h2><strong>&nbsp;Callsign&nbsp;</strong></h2>
-</td>
-<td style="text-align: center;">
-<h2>&nbsp;<strong>DMR ID</strong>&nbsp; </h2>
-</td>
-<td style="text-align: center;">
-<h2>&nbsp;<strong>Bulletin</strong>&nbsp;</h2>
-</td>
-<td style="text-align: center;">
-<h2>&nbsp;<strong>Local Time</strong>&nbsp;</h2>
-</td>
-</tr>
-'''
-    loc_hdr = '''
-<tr>
-<td style="text-align: center;">
-<h2><strong>&nbsp;Callsign&nbsp;</strong></h2>
-</td>
-<td style="text-align: center;">
-<h2>&nbsp;<strong>Latitude</strong>&nbsp; </h2>
-</td>
-<td style="text-align: center;">
-<h2>&nbsp;<strong>Longitude</strong>&nbsp;</h2>
-</td>
-<td style="text-align: center;">
-<h2>&nbsp;<strong>Local Time</strong>&nbsp;</h2>
-</td>
-</tr>
-'''
-    
-    for e in dash_bb:
-        tmp_bb = tmp_bb + '''<tr>
-<td style="text-align: center;"><strong>&nbsp;''' + e['call'] + '''&nbsp;</strong></td>
-<td style="text-align: center;">''' + str(e['dmr_id']) + '''</td>
-<td style="text-align: center;"><strong>&nbsp;''' + e['bulliten'] + '''&nbsp;</strong></td>
-<td style="text-align: center;">&nbsp;''' + e['time'] + '''&nbsp;</td>
-</tr>'''
-    for e in dash_loc:
-        tmp_loc = tmp_loc + '''<tr>
-<td style="text-align: center;"><strong>&nbsp;''' + e['call'] + '''&nbsp;</strong></td>
-<td style="text-align: center;">&nbsp;''' + str(e['lat']) + '''&nbsp;</td>
-<td style="text-align: center;">&nbsp;''' + str(e['lon']) + '''&nbsp;</td>
-<td style="text-align: center;">&nbsp;''' + e['time'] + '''&nbsp;</td>
-</tr>'''
-    return str('<h1 style="text-align: center;">Bulletin Board</h1>' + tbl_hdr + bb_hdr + tmp_bb + tbl_ftr + str('<h1 style="text-align: center;">Positions Received</h1>') + tbl_hdr + loc_hdr + tmp_loc + tbl_ftr)
+
+def get_loc_data():
+    try:
+        dash_loc = ast.literal_eval(os.popen('cat /tmp/gps_data_user_loc.txt').read())
+        tmp_loc = ''
+        loc_hdr = '''
+    <tr>
+    <td style="text-align: center;">
+    <h2><strong>&nbsp;Callsign&nbsp;</strong></h2>
+    </td>
+    <td style="text-align: center;">
+    <h2>&nbsp;<strong>Latitude</strong>&nbsp; </h2>
+    </td>
+    <td style="text-align: center;">
+    <h2>&nbsp;<strong>Longitude</strong>&nbsp;</h2>
+    </td>
+    <td style="text-align: center;">
+    <h2>&nbsp;<strong>Local Time</strong>&nbsp;</h2>
+    </td>
+    </tr>
+    '''
+        for e in dash_loc:
+            tmp_loc = tmp_loc + '''<tr>
+    <td style="text-align: center;"><a href="https://aprs.fi/''' + e['call'] + '''"><strong>''' + e['call'] + '''</strong></a></td>
+    <td style="text-align: center;"><strong>&nbsp;''' + str(e['lat']) + '''&nbsp;</strong></td>
+    <td style="text-align: center;"><strong>&nbsp;''' + str(e['lon']) + '''&nbsp;</strong></td>
+    <td style="text-align: center;">&nbsp;''' + e['time'] + '''&nbsp;</td>
+    </tr>'''
+        return str(str('<h1 style="text-align: center;">Positions Received</h1>') + tbl_hdr + loc_hdr + tmp_loc + tbl_ftr)
+    except:
+        return str('<h1 style="text-align: center;">No data</h1>')
+
+
+def get_bb_data():
+    try:
+        dash_bb = ast.literal_eval(os.popen('cat /tmp/gps_data_user_bb.txt').read())
+        tmp_bb = ''
+        
+        bb_hdr = '''
+    <tr>
+    <td style="text-align: center;">
+    <h2><strong>&nbsp;Callsign&nbsp;</strong></h2>
+    </td>
+    <td style="text-align: center;">
+    <h2>&nbsp;<strong>DMR ID</strong>&nbsp; </h2>
+    </td>
+    <td style="text-align: center;">
+    <h2>&nbsp;<strong>Bulletin</strong>&nbsp;</h2>
+    </td>
+    <td style="text-align: center;">
+    <h2>&nbsp;<strong>Local Time</strong>&nbsp;</h2>
+    </td>
+    </tr>
+    '''
+        
+        for e in dash_bb:
+            tmp_bb = tmp_bb + '''<tr>
+    <td style="text-align: center;"><strong>&nbsp;''' + e['call'] + '''&nbsp;</strong></td>
+    <td style="text-align: center;">''' + str(e['dmr_id']) + '''</td>
+    <td style="text-align: center;"><strong>&nbsp;''' + e['bulliten'] + '''&nbsp;</strong></td>
+    <td style="text-align: center;">&nbsp;''' + e['time'] + '''&nbsp;</td>
+    </tr>'''
+
+        return str('<h1 style="text-align: center;">Bulletin Board</h1>' + tbl_hdr + bb_hdr + tmp_bb + tbl_ftr)
+    except:
+        return str('<h1 style="text-align: center;">No data</h1>')
 
 @app.route('/')
-def dash():
-    #return 'Hello, World!'
-    return get_data()
+def index():
+    #return get_data()
+    return render_template('index.html', title = dashboard_title, logo = logo)
+@app.route('/bulletin_board')
+def dash_bb():
+    return get_bb_data()
+    #return render_template('index.html', data = str(get_data()))
+@app.route('/positions')
+def dash_loc():
+    return get_loc_data()
+    #return render_template('index.html', data = str(get_data()))
